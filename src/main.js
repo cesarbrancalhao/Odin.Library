ee11f3c2b38f2ef349108cbd382905279aa576f9
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-01-20 13:14:33 -0300

    Update: refactor + basic validations

--- src/main.js
+++ src/main.js
@@ -21,10 +21,10 @@ function Book(title, publisher, author, pages, status) {
         const read = this.status.includes('r') ? 'read' : 'not read';
         return `${this.title} by ${this.author}, ${this.pages} pages, ${read}.`;
     }
-}
+};
 
 const library = [
-    new Book('The Great Gatsby', 'Charles Scribners', 'F. Scott Fitzgerald', 180, 'n'),
+    new Book('The Great Gatsby', 'Charles Scribner`s', 'F. Scott Fitzgerald', 180, 'n'),
     new Book('To Kill a Mockingbird', 'Grand Central Publishing', 'Harper Lee', 281, 'r'),
 ];
 
@@ -38,16 +38,44 @@ let errors = [];
 
 let addBook = (title, publisher, author, pages, status) => {
     
-    const validate = validateBook(title, publisher, author, pages, status);
+    const valid = validateBook(title, publisher, author, pages, status);
 
-    if (!validate) {
+    if (valid === true) {
         const newBook = new Book(title, publisher, author, pages, status);
         library.push(newBook);
         return;
-    };
+    }
 
     return errors;
-}
+};
+
+/**
+ * Validates the book information.
+ *
+ * @return {Array} errors - If the data is invalid.
+ * @return {true} - If the data is valid
+ */
+const validateBook = (title, publisher, author, pages, status) => {
+    let valid = true;
+
+    if (/[^a-zA-Z.' ]/.test(title))
+        {errors.push("Invalid title."); valid = false;}
+
+    if (/[^a-zA-Z.' ]/.test(publisher))
+        {errors.push("Invalid publisher."); valid = false;}
+
+    if (/[^a-zA-Z.' ]/.test(author))
+        {errors.push("Invalid author."); valid = false;}
+
+    if (isNaN(pages) || pages <= 0)
+        {errors.push("Invalid pages."); valid = false;}
+
+    if (status !== 'r' && status !== 'n')
+        {errors.push("Invalid status."); valid = false;}
+
+    return valid ? true : errors;
+};
+
 
 /**
  * Retrieves a list of books.
@@ -56,20 +84,20 @@ let addBook = (title, publisher, author, pages, status) => {
  * @return {Array} - Returns an array of books.
  */
 
-let listBooks = (option) => {
-    const filterOps = {
+const listBooks = (option) => {
+    const filters = {
         'w': 'publisher',
         'a': 'author',
         'p': 'pages',
         's': 'status',
-    };
+    }
 
-    const valid = isValid(option);
+    const isValidOption = isValid(option);
     
-    const filter = valid ? filterOps[option] : false;
+    const selectedFilter = isValidOption ? filters[option] : false;
 
-    return !filter ? library : sortBooks(filter, option);
-}
+    return !selectedFilter ? library : sortBooks(selectedFilter, option);
+};
 
 /**
  * Sorts the library of books based on the given option.
@@ -79,9 +107,10 @@ let listBooks = (option) => {
  * @return {Array} - The sorted library of books.
  */
 
-let sortBooks = (filter, option) => {
+const sortBooks = (filter, option) => {
     
-    return option === 'p' ?
-              library.sort((a, b) => a[filter] - b[filter])
-            : library.sort((a, b) => a[filter].localeCompare(b[filter]));
-}
+    if (option === 'p')
+        return library.sort((a, b) => a[filter] - b[filter]);
+    
+    return library.sort((a, b) => a[filter].localeCompare(b[filter]));
+};
