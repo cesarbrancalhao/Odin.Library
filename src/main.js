31c165ca01a2c3403bd037fa142bb76be700e320
cesarbrancalhao <58019405+cesarbrancalhao@users.noreply.github.com>
2024-01-23 15:49:46 -0300

    Added: Form, validations, reset

--- src/main.js
+++ src/main.js
@@ -1,3 +1,42 @@
+/* --- Variables --- */
+
+const updateBtn = document.querySelector('#updateButton');
+const addBookBtn = document.querySelector('#addBookButton');
+const modal = document.querySelector('#openModalForm');
+const bookForm = document.querySelector('#bookForm');
+const closeModalBtn = document.querySelector('#closeModalForm');
+const container = document.querySelector('#container');
+const errorHelper = document.querySelector('#errorHelper');
+let isOpenModal = false;
+let errors = [];
+let library = [
+    new Book('The Great Gatsby', 'Charles Scribner`s', 'F. Scott Fitzgerald', 180, 'n'),
+    new Book('Ao Kill a Mockingbird', 'Grand Central Publishing', 'Harper Lee', 281, 'r'),
+];
+
+/* --- Listeners --- */
+
+updateBtn.addEventListener('click', () => {
+    fillPageBooks();
+})
+
+addBookBtn.addEventListener('click', () => {
+    openModalForm();
+});
+
+closeModalBtn.addEventListener('click', () => {
+    closeModalForm();
+});
+
+container.addEventListener('click', () => {
+    fillPageBooks();
+});
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    submitBook();
+});
+
 /* --- Book section --- */
 
 /**
@@ -26,26 +65,14 @@ function Book(title, publisher, author, pages, status, description = "") {
     }
 };
 
-const library = [
-    new Book('The Great Gatsby', 'Charles Scribner`s', 'F. Scott Fitzgerald', 180, 'n'),
-    new Book('Ao Kill a Mockingbird', 'Grand Central Publishing', 'Harper Lee', 281, 'r'),
-];
-
-let errors = [];
-
-const addBookBtn = document.querySelector('#addBookButton');
-
-// Calls addBook modal
-addBookBtn.addEventListener('click', () => {
-    modalAddBook();
-});
+/* --- Form section --- */
 
 /**
  * Adds a book to the library.
  * 
  * @param {string} status - The status of the book ('r' for read, 'n' for not read).
  */
-let addBook = (title, publisher, author, pages, status, description = "") => {
+const addBook = (title, publisher, author, pages, status, description = "") => {
     
     errors = [];
 
@@ -61,105 +88,68 @@ let addBook = (title, publisher, author, pages, status, description = "") => {
     return false;
 };
 
-const modalAddBook = () => {
+/**
+ * Opens the modal form by removing the 'hidden' class.
+ *
+ */
+const openModalForm = () => {
 
-    let modal = document.querySelector('#modalAddBook');
+    if (isOpenModal){
+        closeModalForm();
+        return;
+    }
+
+
+    container.classList.add('hidden');
+    updateBtn.classList.add('hidden');
+    addBookBtn.classList.add('hidden');
+
     modal.classList.remove('hidden');
-
-    // Add modal here
-
-    modal.classList.add('hidden');
+    isOpenModal = true;
 }
 
-/* --- List section --- */
+const closeModalForm = () => {
 
-const container = document.querySelector('#container');
+    container.classList.remove('hidden');
+    updateBtn.classList.remove('hidden');
+    addBookBtn.classList.remove('hidden');
 
-// Update list button
-container.addEventListener('click', () => {
-    fillPageBooks('t');
-});
+    modal.classList.add('hidden');
+    isOpenModal = false;
+}
 
 /**
- * Retrieves a list of books.
+ * Submits a book by retrieving input values from the document, 
+ * adding the book using the addBook function, and updating the page 
+ * with the book list if successful.
  *
- * @param {string} option - The ordering filter of the list ('w' for publisher, 'a' for author, 'p' for pages, 's' for status).
- * @return {Array} - Returns an array of books.
+ * @return {boolean} true and refresh the list if the book is successfully added, false otherwise.
  */
-const listBooks = (option) => {
-    
-    errors = [];
+const submitBook = () => {
+    const { value: title } = document.querySelector('#title');
+    const { value: publisher } = document.querySelector('#publisher');
+    const { value: author } = document.querySelector('#author');
+    const { value: pages } = document.querySelector('#pages');
+    const { value: status } = document.querySelector('#status');
+    const { value: description } = document.querySelector('#description');
 
-    const filters = {
-        't': 'title',
-        'w': 'publisher',
-        'a': 'author',
-        'p': 'pages',
-        's': 'status',
+    const put = addBook(title, publisher, author, pages, status, description);
+    
+    if (put) {
+        closeModalForm();
+        fillPageBooks();
+        resetForm();
     }
-    
-    const selectedFilter = option === 'd' ? filters[option] : false;
+}
 
-    return !selectedFilter ? library : sortBooks(selectedFilter, option);
-};
-
-/**
- * Sorts the library of books based on the given option.
- *
- * @param {string} filter - The atribute to sort.
- * @param {string} option - The sorting option.
- * @return {Array} - The sorted library of books.
- */
-const sortBooks = (filter, option) => {
-    
-    if (option === 'p')
-        return library.sort((a, b) => a[filter] - b[filter]);
-    
-    return library.sort((a, b) => a[filter].localeCompare(b[filter]));
-};
-
-
-/**
- * Update the page's list of books.
- * 
- * @param {string} option - The ordering filter of the list.
- * @function listBooks - Retrieves the list of books.
- */
-let fillPageBooks = (option) => {
-    
-    let books = listBooks(option);
-
-    let hasErrors = checkErrors();
-
-    if (hasErrors)
-        return;
-
-    
-    container.innerHTML = '';
-
-    books.forEach((book) => {
-
-        let bookDiv = document.createElement('a');
-        bookDiv.classList.add('rounded', 'bg-gray-100', 'p-5', 'm-5', 'shadow-lg', 'hover:bg-blue-400');
-
-        let titleElement = document.createElement('h2');
-        titleElement.classList.add('font-bold', 'text-xl');
-        titleElement.textContent = book.title;
-
-        let authorElement = document.createElement('p');
-        authorElement.textContent = book.author;
-
-        let infoElement = document.createElement('p');
-        infoElement.classList.add('mt-2');
-        infoElement.textContent = book.info();
-
-        bookDiv.appendChild(titleElement);
-        bookDiv.appendChild(authorElement);
-        bookDiv.appendChild(infoElement);
-
-        container.appendChild(bookDiv);
-    })
-};
+const resetForm = () => {
+    document.querySelector('#title').value = '';
+    document.querySelector('#publisher').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#status').value = 'n';
+    document.querySelector('#description').value = '';
+}
 
 
 /* --- Errors section --- */
@@ -185,7 +175,7 @@ const validateBook = (title, publisher, author, pages, status, description = "")
     if (isNaN(pages) || pages <= 0)
         {errors.push("Invalid number of pages."); valid = false;}
 
-    if (status !== 'r' || status !== 'n')
+    if (status !== 'r' && status !== 'n')
         {errors.push("Invalid characters in status."); valid = false;}
 
     if (/[^a-zA-Z.' ]/.test(description))
@@ -197,8 +187,6 @@ const validateBook = (title, publisher, author, pages, status, description = "")
     return valid ? true : errors;
 };
 
-
-
 /**
  * Check if there are errors in the array display them if any.
  *
@@ -207,7 +195,6 @@ const validateBook = (title, publisher, author, pages, status, description = "")
 let checkErrors = () => {
     if (errors.length > 0) {
         errors.forEach((error) => {
-            let errorHelper = document.querySelector('#errorHelper');
             errorHelper.textContent = "";
             errorHelper.textContent += `${error}\n`;
         });
@@ -216,6 +203,47 @@ let checkErrors = () => {
     return false;
 }
 
+/* --- List section --- */
+
+/**
+ * Update the page's list of books.
+ * 
+ * @function listBooks - Retrieves the list of books.
+ */
+const fillPageBooks = () => {
+
+    let books = library;
+
+    let hasErrors = checkErrors();
+
+    if (hasErrors)
+    {errors = []; return;}
+
+    container.innerHTML = '';
+
+    books.forEach((book) => {
+
+        let bookDiv = document.createElement('a');
+        bookDiv.classList.add('rounded', 'bg-gray-100', 'p-5', 'm-5', 'shadow-lg', 'hover:bg-blue-400');
+
+        let titleElement = document.createElement('h2');
+        titleElement.classList.add('font-bold', 'text-xl');
+        titleElement.textContent = book.title;
+
+        let authorElement = document.createElement('p');
+        authorElement.textContent = book.author;
+
+        let infoElement = document.createElement('p');
+        infoElement.classList.add('mt-2');
+        infoElement.textContent = book.info();
+
+        bookDiv.appendChild(titleElement);
+        bookDiv.appendChild(authorElement);
+        bookDiv.appendChild(infoElement);
+
+        container.appendChild(bookDiv);
+    });
+};
 
 /* --- Init --- */
 
